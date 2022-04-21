7bc1470348e35b74d62684e7d4e0fbcd09a356d8
ItzValidity <chaisseandkamh@gmail.com>
2022-04-21 01:47:26 +0000

    Created the filter code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+
+const correctStatement = 102002;
+const manyDifferentStatements = [102002, 83927384, 23213233, 23432546];
+
+const filtered = manyDifferentStatements.filter(function(value) {
+    return value == correctStatement;
+});
+
+console.log(filtered);
